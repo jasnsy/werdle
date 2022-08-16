6e07f9624174cfe4192de082789eca43df522096
Jason <jasnsy@gmail.com>
2022-08-16 13:33:45 -0600

    update wording

--- src/components/GameOver.js
+++ src/components/GameOver.js
@@ -10,7 +10,7 @@ function GameOver() {
 
   return (
     <div className='gameOver'>
-        <h1>{gameOver.guessedWord ? "We did it fam!" : "You Suck!"}</h1>
+        <h1>{gameOver.guessedWord ? "We did it!" : "Sorry, you didn't get it..."}</h1>
         {!gameOver.guessedWord && (<h2>The correct word was: {correctWord}</h2>)}
         {gameOver.guessedWord && <h2>You guessed it on attempt {currAttempt.attempt}</h2>}
         <button className='button' onClick={refreshPage}>Play Again?</button>
